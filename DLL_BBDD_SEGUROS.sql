c592c3d7ffe9982ba08c7f89bdc56b7abb315aae
Sebastián Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2024-08-13 21:15:12 -0500

    Actualización de DLL de base de datos de seguros

--- DLL_BBDD_SEGUROS.sql
+++ DLL_BBDD_SEGUROS.sql
@@ -1,138 +1,132 @@
--- Active: 1723121637486@@127.0.0.1@3306
-CREATE DATABASE BBDD_SEGUROS;
+-- Creación de tabla Cliente
+CREATE TABLE Cliente(
+    Cli_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Cli_Nombre VARCHAR(30),
+    Cli_Documento VARCHAR(20),
+    Cli_Direccion VARCHAR(40),
+    Cli_Telefono VARCHAR(15),
+    Cli_Profesion VARCHAR(25),
+    Cli_Edad VARCHAR(3)
+);
 
- USE BBDD_SEGUROS;
-   
+-- Creación de tabla Beneficiario
+CREATE TABLE Beneficiario(
+    Ben_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Ben_Nombre VARCHAR(30)   
+);
 
-    CREATE TABLE Categoria_Automovil(
-        CaA_ID INT(5) AUTO_INCREMENT PRIMARY KEY,
-        CaA_Nombre VARCHAR(20) 
-    );
+-- Creación de tabla Agente
+CREATE TABLE Agente(
+    Age_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Age_Nombre VARCHAR(30), 
+    Age_Direccion VARCHAR(40),
+    Age_Telefono VARCHAR(15),
+    Age_Documento VARCHAR(20)
+);
 
-    CREATE TABLE Cobertura(
-        Cob_ID INT(5) AUTO_INCREMENT PRIMARY KEY,
-        Cob_Tipo VARCHAR(20) 
-    );
+-- Creación de tabla Automovil
+CREATE TABLE Automovil(
+    Aut_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Aut_Categoria VARCHAR(20),
+    Aut_Annos INT(3),
+    Aut_Matricula VARCHAR(6)    
+);
 
-    CREATE TABLE Conductor(
-        Con_ID INT(5) AUTO_INCREMENT PRIMARY KEY,
-        Con_Nombre VARCHAR(20), 
-        Con_Edad VARCHAR(20)
-    );
+-- Creación de tabla Conductor
+CREATE TABLE Conductor(
+    Con_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Con_Nombre VARCHAR(30),
+    Con_Edad VARCHAR(3)
+);
 
-    CREATE TABLE Inmueble(
-        Inm_ID INT(5) AUTO_INCREMENT PRIMARY KEY,
-        Inm_Direccion VARCHAR(40),
-        Inm_Valor VARCHAR(20)
-    );
+-- Creación de tabla Inmueble
+CREATE TABLE Inmueble(
+    Inm_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Inm_Direccion VARCHAR(40),
+    Inm_Valor VARCHAR(40)
+);
 
-    CREATE TABLE Agente(
-        Age_ID INT(5) AUTO_INCREMENT PRIMARY KEY,
-        Age_Nombre VARCHAR(25),
-        Age_Documento VARCHAR(15),
-        Age_Direccion VARCHAR(40),
-        Age_Telefono VARCHAR(20)
-    );
+-- Creación de tabla que contiene los bienes del inmueble
+CREATE TABLE Bienes_Inmueble(
+    BiI_ID INT AUTO_INCREMENT PRIMARY KEY,
+    BiI_Nombre VARCHAR(30),
+    BiI_Valor VARCHAR(30),
+    FK_Inm_ID INT NOT NULL,
+    FOREIGN KEY FK_Inm_ID1(FK_Inm_ID) REFERENCES Inmueble(Inm_ID) 
+);
 
-    CREATE TABLE Cliente(
-        Cli_ID INT(5) AUTO_INCREMENT PRIMARY KEY,
-        Cli_Nombre VARCHAR(25),
-        Cli_Documento VARCHAR(15),
-        Cli_Direccion VARCHAR(40),
-        Cli_Telefono VARCHAR(20),
-        Cli_Edad VARCHAR(4),
-        Cli_Profesion VARCHAR(20)
-    );
-   
-    CREATE TABLE Beneficiario(
-        Ben_ID INT(5) AUTO_INCREMENT PRIMARY KEY,
-        Ben_Nombre VARCHAR(25)
-    );
+-- Creación de tabla de seguro de vida
+CREATE TABLE Seguro_Vida(
+    SeV_ID INT AUTO_INCREMENT PRIMARY KEY,
+    SeV_CoberturaEconomica VARCHAR(20),
+    SeV_Vigente BOOLEAN
+);
 
-    CREATE TABLE Oferta(
-        Ofe_ID INT(5) AUTO_INCREMENT PRIMARY KEY,
-        Ofe_Valor VARCHAR(20),
-        Ofe_Prima VARCHAR(20),
-        Ofe_Fecha DATE,
-        FK_Age_ID INT NOT NULL,
-        FOREIGN KEY FK_Age_ID1(FK_Age_ID) REFERENCES Agente(Age_ID)
-    );
+-- Creación de tabla de seguro de automóvil
+CREATE TABLE Seguro_Automovil(
+    SeA_ID INT AUTO_INCREMENT PRIMARY KEY,
+    SeA_Recargos FLOAT(20), 
+    SeA_Descuentos FLOAT(20),
+    SeA_Cobertura VARCHAR(20),
+    SeA_Vigente BOOLEAN,
+    FK_Aut_ID INT NOT NULL,
+    FK_Con_ID INT NOT NULL,
+    FOREIGN KEY FK_Aut_ID1(FK_Aut_ID) REFERENCES Automovil(Aut_ID),
+    FOREIGN KEY FK_Con_ID1(FK_Con_ID) REFERENCES Conductor(Con_ID)
+);
 
-    CREATE TABLE Bienes_Inmueble(
-        BieI_ID INT(5) AUTO_INCREMENT PRIMARY KEY,
-        BieI_Nombre VARCHAR(20),
-        BieI_Valor VARCHAR(20),
-        FK_Inm_ID INT NOT NULL,
-        FOREIGN KEY FK_Inm_ID1(FK_Inm_ID) REFERENCES Inmueble(Inm_ID)
-    );
-
-    CREATE TABLE Riesgos_Auxiliares(
-        RiA_ID INT (5) AUTO_INCREMENT PRIMARY KEY,
-        RiA_Nombre VARCHAR (20),
-        RiA_Valor INT(15),
-        FK_Inm_ID INT NOT NULL,
-        FOREIGN KEY FK_Inm_ID4(FK_Inm_ID) REFERENCES Inmueble(Inm_ID)
-    )
-
-    CREATE TABLE Seguro_Hogar(
-        SeH_ID INT(5) AUTO_INCREMENT PRIMARY KEY,
-        SeH_FechaContrato DATE,
-        FK_Inm_ID INT NOT NULL,
-        FK_Ofe_ID INT NOT NULL,
-        FOREIGN KEY FK_Inm_ID(FK_Inm_ID) REFERENCES Inmueble(Inm_ID),
-        FOREIGN KEY FK_Ofe_ID(FK_Ofe_ID) REFERENCES Oferta(Ofe_ID)
-    );
-
-    CREATE TABLE Seguro_Vida(
-        SeV_ID INT(5) AUTO_INCREMENT PRIMARY KEY,
-        SeV_CoberturaEconomica VARCHAR(20),
-        FK_Ofe_ID INT NOT NULL,
-        FK_Cli_ID INT NOT NULL,
-        FOREIGN KEY FK_Ofe_ID1(FK_Ofe_ID) REFERENCES Oferta(Ofe_ID),
-        FOREIGN KEY FK_Cli_ID1(FK_Cli_ID) REFERENCES Cliente(Cli_ID)
-    );
-      CREATE TABLE Seguro_Automovil(
-        SeA_ID INT(5) AUTO_INCREMENT PRIMARY KEY,
-        SeA_Recargos VARCHAR(20),
-        SeA_Descuentos VARCHAR(20),
-        FK_Ofe_ID INT NOT NULL,
-        FK_Cob_ID INT NOT NULL,
-        FK_Con_ID INT NOT NULL,
-        FOREIGN KEY FK_Ofe_ID2(FK_Ofe_ID) REFERENCES Oferta(Ofe_ID),
-        FOREIGN KEY FK_Cob_ID2(FK_Cob_ID) REFERENCES Cobertura(COb_ID),
-        FOREIGN KEY FK_Con_ID2(FK_Con_ID) REFERENCES Conductor(Con_ID)
-    );
-
-    CREATE TABLE Automovil(
-        Aut_ID INT(5) AUTO_INCREMENT PRIMARY KEY,
-        Aut_Matricula VARCHAR(6),
-        Aut_Anos INT(5),
-        FK_CaA_ID INT NOT NULL,
-        FK_SeA_ID INT NOT NULL,
-        FOREIGN KEY FK_CaA_ID(FK_CaA_ID) REFERENCES Categoria_Automovil(CaA_ID),
-        FOREIGN KEY FK_SeA_ID(FK_SeA_ID) REFERENCES Seguro_Automovil(SeA_ID)
-    );
-
-  
-
-    CREATE TABLE Contrato(
-        FK_Cli_ID INT NOT NULL,
-        FK_SeH_ID INT NOT NULL,
-        FK_SeV_ID INT NOT NULL,
-        FK_SeA_ID INT NOT NULL,
-        FK_Age_ID INT NOT NULL,
-        FK_Ben_ID INT NOT NULL,
-        FOREIGN KEY FK_Cli_ID3(FK_Cli_ID) REFERENCES Cliente(Cli_ID),
-        FOREIGN KEY FK_SeH_ID3(FK_SeH_ID) REFERENCES Seguro_Hogar(SeH_ID),
-        FOREIGN KEY FK_SeV_ID3(FK_SeV_ID) REFERENCES Seguro_Vida(SeV_ID),
-        FOREIGN KEY FK_SeA_ID3(FK_SeA_ID) REFERENCES Seguro_Automovil(SeA_ID),
-        FOREIGN KEY FK_Age_ID3(FK_Age_ID) REFERENCES Agente(Age_ID),
-        FOREIGN KEY FK_Ben_ID3(FK_Ben_ID) REFERENCES Beneficiario(Ben_ID)
-    );
-  
-
-
+-- Creación de tabla de seguro de hogar
+CREATE TABLE Seguro_Hogar(
+    SeH_ID INT AUTO_INCREMENT PRIMARY KEY,
+    SeH_RiesgoAuxiliar VARCHAR(15),
+    SeH_Vigente BOOLEAN,
+    FK_Inm_ID INT NOT NULL,
+    FOREIGN KEY FK_Inm_ID2(FK_Inm_ID) REFERENCES Inmueble(Inm_ID)
+);
     
+-- Creación de tabla que contiene contratos relacionados con seguros de vida
+CREATE TABLE Contrato_SeguroVida(
+    CSV_ID INT AUTO_INCREMENT PRIMARY KEY,
+    CSV_FechaContrato DATE,
+    CSV_Prima INT(20),
+    FK_Cli_ID INT NOT NULL,
+    FK_Ben_ID INT NOT NULL,
+    FK_SeV_ID INT NOT NULL,
+    FK_Age_ID INT NOT NULL,
+    FOREIGN KEY FK_Cli_ID2(FK_Cli_ID) REFERENCES Cliente(Cli_ID),
+    FOREIGN KEY FK_Ben_ID2(FK_Ben_ID) REFERENCES Beneficiario(Ben_ID),
+    FOREIGN KEY FK_SeV_ID2(FK_SeV_ID) REFERENCES Seguro_Vida(SeV_ID),
+    FOREIGN KEY FK_Age_ID2(FK_Age_ID) REFERENCES Agente(Age_ID)
+);
 
+-- Creación de tabla que contiene contratos relacionados con seguros de automóvil
 
+CREATE TABLE Contrato_SeguroAutomovil(
+    CSA_ID INT AUTO_INCREMENT PRIMARY KEY,
+    CSA_FechaContrato DATE,
+    CSA_Prima INT(20),
+    FK_Cli_ID INT NOT NULL,
+    FK_Ben_ID INT NOT NULL,
+    FK_SeA_ID INT NOT NULL,
+    FK_Age_ID INT NOT NULL,
+    FOREIGN KEY FK_Cli_ID3(FK_Cli_ID) REFERENCES Cliente(Cli_ID),
+    FOREIGN KEY FK_Ben_ID3(FK_Ben_ID) REFERENCES Beneficiario(Ben_ID),
+    FOREIGN KEY FK_SeA_ID3(FK_SeA_ID) REFERENCES Seguro_Automovil(SeA_ID),
+    FOREIGN KEY FK_Age_ID3(FK_Age_ID) REFERENCES Agente(Age_ID)
+);
 
+-- Creación de tabla que contiene contratos relacionados con seguros de hogar
+
+CREATE TABLE Contrato_SeguroHogar(
+    CSH_ID INT AUTO_INCREMENT PRIMARY KEY,
+    CSH_FechaContrato DATE,
+    CSH_Prima INT(20),
+    FK_Cli_ID INT NOT NULL,
+    FK_Ben_ID INT NOT NULL,
+    FK_SeH_ID INT NOT NULL,
+    FK_Age_ID INT NOT NULL,
+    FOREIGN KEY FK_Cli_ID4(FK_Cli_ID) REFERENCES Cliente(Cli_ID),
+    FOREIGN KEY FK_Ben_ID4(FK_Ben_ID) REFERENCES Beneficiario(Ben_ID),
+    FOREIGN KEY FK_SeH_ID4(FK_SeH_ID) REFERENCES Seguro_Hogar(SeH_ID),
+    FOREIGN KEY FK_Age_ID4(FK_Age_ID) REFERENCES Agente(Age_ID)
+);
